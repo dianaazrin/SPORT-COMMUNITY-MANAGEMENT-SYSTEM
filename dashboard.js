2655259bf0b3b07f041c29f26ee1d303876f241d
dianaazrin <2022812648@student.uitm.edu.my>
2024-05-10 02:12:02 +0800

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,29 @@
+function redirectToArenaBooking() {
+    // Redirect to the arena booking page
+    window.location.href = "arena.html";
+}
+
+        function showArena() {
+            // Code to show arena-related content
+        }
+
+        function showEquipment() {
+            // Code to show equipment-related content
+        }
+
+        function showInformation() {
+            // Code to show information-related content
+        }
+
+        function viewMoreArena() {
+            // Code to handle view more for arena booking
+        }
+
+        function viewMoreEquipment() {
+            // Code to handle view more for equipment booking
+        }
+        
+        function redirectToHome() {
+            window.location.href = "dashboard.html";
+        }
+   
---
+++ login.js
@@ -0,0 +1,6 @@
+function redirectToDashboard(event) {
+    event.preventDefault(); // prevent default form submission
+    // Redirect to the dashboard page
+    window.location.href = "dashboard.html";
+}
+
